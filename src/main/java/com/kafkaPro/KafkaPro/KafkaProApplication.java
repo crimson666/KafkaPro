4fe0a61ef9dcefbf38bea1f7d780f314032c2f07
crimson666 <ankurbsws@gmail.com>
2024-08-31 22:14:52 +0530

    Kafka project demo

---
+++ src/main/java/com/kafkaPro/KafkaPro/KafkaProApplication.java
@@ -0,0 +1,13 @@
+package com.kafkaPro.KafkaPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kafkaPro/KafkaPro/KafkaProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafkaPro.KafkaPro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
