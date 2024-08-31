350d9a58e8b1441d65d758038c8803213e610ccc
crimson666 <ankurbsws@gmail.com>
2024-08-31 23:04:37 +0530

    little stuff

---
+++ kafka-producer-wiki/src/main/java/net/springproj/springBootProducerApplication.java
@@ -0,0 +1,11 @@
+package net.springproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class springBootProducerApplication {
+    public static void main(String[] args){
+        SpringApplication.run(springBootProducerApplication.class);
+    }
+}
