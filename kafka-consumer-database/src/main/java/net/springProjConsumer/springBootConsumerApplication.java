b84814bc6820f1a3a395580252a2d570032d604c
crimson666 <ankurbsws@gmail.com>
2024-09-03 22:42:45 +0530

    Consumer

---
+++ kafka-consumer-database/src/main/java/net/springProjConsumer/springBootConsumerApplication.java
@@ -0,0 +1,11 @@
+package net.springProjConsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class springBootConsumerApplication {
+    public static void main(String[] args){
+        SpringApplication.run(springBootConsumerApplication.class);
+    }
+}
